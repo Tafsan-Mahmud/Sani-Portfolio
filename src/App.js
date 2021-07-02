31734801d64e78318169b01155fe7b8ce7663d60
Tafsan Mahmud Nobin <tafsanmahmudnobin090@gmail.com>
2021-07-02 09:35:38 +0600

    navbar complete

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { Button } from 'react-bootstrap';
+import NavBar from './Components/NavBar/NavBar';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         <code>src/App.js</code>
-        </p>
-        {/* <Button variant="primary">Primary</Button> */}
-      </header>
+    <div>
+      <Router>
+      <NavBar></NavBar>
+      <Switch>
+        <Route path="/home">
+        
+        </Route>
+      </Switch>
+      </Router>
     </div>
   );
 }
