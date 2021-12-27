a85eeda39228e8ce44e8e59b442808e43ef50f15
Tafsan Mahmud Nobin <tafsanmahmudnobin090@gmail.com>
2021-12-27 21:41:53 +0600

    add service section half

--- src/Components/Dashboard/AddService/AddService.js
+++ src/Components/Dashboard/AddService/AddService.js
@@ -1,20 +1,119 @@
 import React from 'react';
 import Sidebar from '../Sidebar/Sidebar';
 import './AddService.css';
+import srvcPkCmng from './IMG_4203-01.jpg';
+import srvcPkCmng2 from './IMG_2258-02.jpg';
+import { useState, useEffect } from 'react';
 
 const AddService = () => {
+
+    const checkIsComing = false;
+    const todoData = JSON.parse(localStorage.getItem('TodoList'))
+    const [todoFkDta, setTodoFkDta] = useState([]);
+    const [numID, setNumID] = useState(1);
+    console.log(numID)
+    console.log(todoFkDta)
+
+    // console.log(todoFkDta)
+    const handleTodoSet = () => {
+        const todo = document.getElementById('srvs-todo').value;
+        setNumID(numID + 1)
+        setTodoFkDta([...todoFkDta, { id: numID, Service: todo }])
+    }
+    useEffect(() => {
+        localStorage.setItem("TodoList", JSON.stringify(todoFkDta))
+
+    }, [todoFkDta]);
+
+
+
+    const fkSrvsData = [
+        {
+            id: 1,
+            Service: 'Youtube Marketing'
+        },
+        {
+            id: 2,
+            Service: 'Linkedin'
+        },
+        {
+            id: 3,
+            Service: 'Facebook Likes'
+        },
+        {
+            id: 4,
+            Service: 'Instagram Followers'
+        },
+        {
+            id: 5,
+            Service: 'SEO Marketing'
+        },
+        {
+            id: 6,
+            Service: 'Marketing'
+        }
+
+    ];
+
     return (
         <div id="addService">
-            <Sidebar></Sidebar>
             <div className="main-add-service-sec">
-                <div className="addservice-content-wrp">
-                    <h1 id="addService-ttl-m">Add Order</h1>
+                <Sidebar></Sidebar>
+                <div className="addservice-content-sec">
+                    <h1 id="addService-ttl-m">Add Service</h1>
                     <div className="add-srvc-content">
-                       
+                        <div className="service-coming-img-sec">
+                            {
+                                checkIsComing && <img id='srvs-incoming-img' src={srvcPkCmng} alt="" />
+                            }
+                            {
+                                checkIsComing === false && <img id='srvs-incoming-img' src={srvcPkCmng2} alt="" />
+                            }
+
+                        </div>
+                        <form className="w-100" action='' autoComplete='off'>
+                            <div className="up-section">
+                                <h4>Choose service image</h4>
+                                <input type="file" name="" id="service-image" />
+                                <label htmlFor="service-image">Upload Photo</label>
+                            </div>
+                            <div className="add-srvc-all-input">
+                                <div className="srvc-input-data">
+                                    <input type="text" required name="Name" />
+                                    <span>Service Name</span>
+                                    <div className="underline-add-srvc"></div>
+                                </div>
+                                {/* <div className="srvc-input-data">
+                                    <input type="text" required name="Name" />
+                                    <span>Service Name</span>
+                                    <div className="underline-add-srvc"></div>
+                                </div> */}
+                                <div className="srvs-todo-add-sec">
+                                    <span>Add Services ({7 - numID})</span>
+                                    <div>
+
+                                        <input id='srvs-todo' type="text" />
+                                        {
+                                            numID === 7 && <button type="button" className='btn-disable' >Add</button>
+                                        }
+                                        {
+                                            numID < 7 && < button type="button" onClick={handleTodoSet}>Add</button>
+                                        }
+                                    </div>
+                                </div>
+                                <div className="srvs-todo-listed-sec">
+                                    {
+                                        todoFkDta.map(data => <li className='srvs-todos-dsgn'>{data.id}-{data.Service}</li>
+                                        )
+                                    }
+                                </div>
+                            </div>
+                            <input style={{marginTop:'20px'}} type="submit" value="Submit" />
+                        </form>
                     </div>
                 </div>
-            </div>
-        </div>
+            </div >
+        </div >
     );
 };
 
--- src/Components/Dashboard/BodyDashboard/BodyDashboard.js
+++ src/Components/Dashboard/BodyDashboard/BodyDashboard.js
@@ -10,19 +10,28 @@ const BodyDashboard = () => {
 
     const [mainTheme, setMainTheme] = useContext(PageTheme);
 
-   
+
     const [lightOrDark, setLightOrDark] = useState(mainTheme);
-   
-    
+    const [counting, setCounting] = useState(0)
+    const startCount = () => {
+        setCounting(counting + 1)
+    }
+    const endCount = () => {
+        if (counting > 5) {
+            setCounting(counting - 1)
+        }
+
+    }
+
     console.log(lightOrDark)
 
-    useEffect(() => { 
+    useEffect(() => {
         const data = JSON.parse(localStorage.getItem('DLMode'));
         // console.log(data)
         // const themeData = () => {
 
         // }
-            setLightOrDark(data)     
+        setLightOrDark(data)
     }, [mainTheme]);
 
     return (
@@ -30,9 +39,15 @@ const BodyDashboard = () => {
             <div className="container-fluid mgnpdn">
                 <Sidebar></Sidebar>
                 <div className="main-dashboard-body">
-                    {/* <h1>{lightOrDark ? 'Now dark' : 'Now Light'}</h1> */}
-                    {/* <button onClick={changeModeDL}>{lightOrDark ? dark : light}</button> */}
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde minus maiores rerum, quasi in officia quisquam! Atque eveniet unde quisquam temporibus modi repudiandae vel aspernatur nostrum, quibusdam adipisci architecto commodi odio autem dolorum veritatis est illo, repellat dolore culpa. Beatae, quas pariatur nam in, iure illum ad eaque quae corporis reprehenderit nulla atque deleniti quos quidem quia voluptatum expedita asperiores! Voluptas itaque consectetur dolore porro, non iste consequatur laudantium distinctio quaerat quos quae provident? Culpa sequi, adipisci quidem mollitia nam magnam unde earum deserunt facere  dolor totam voluptatem impedit eveniet laborum sunt nostrum, eius sit veniam id voluptate. Ipsam culpa veritatis ullam magnam laboriosam quas aliquam corrupti blanditiis delectus sequi unde aspernatur vero nulla quod, natus cumque animi dolor? Officiis saepe beatae, blanditiis architecto repellendus eligendi at molestias suscipit. Voluptatibus ut dolorum debitis molestiae quas harum ea aut, deleniti voluptas incidunt quae fuga quod, nihil qui voluptatem eos perferendis repellendus! Eos quasi distinctio consequatur, recusandae sapiente saepe neque, quod corporis praesentium veritatis optio itaque inventore hic nihil vero dignissimos iusto qui? Unde voluptate quo cum velit saepe architecto nemo dolorum, quibusdam vero, quas alias veritatis porro, explicabo ea provident earum blanditiis et voluptates veniam! Facilis porro fugiat libero ipsam. Praesentium.</p>
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde minus maiores rerum, quasi in officia quisquam! Atque eveniet unde quisquam temporibus modi repudiandae vel aspernatur nostrum, quibusdam adipisci architecto commodi odio autem dolorum veritatis est illo, repellat dolore culpa. Beatae, quas pariatur nam in, iure illum ad eaque quae corporis reprehenderit nulla atque deleniti quos quidem quia voluptatum expedita asperiores! Voluptas itaque consectetur dolore porro, non iste consequatur laudantium distinctio quaerat quos quae provident? Culpa sequi, adipisci quidem mollitia nam magnam unde earum deserunt facere  dolor totam voluptatem impedit eveniet laborum sunt nostrum, eius sit veniam id voluptate. Ipsam culpa veritatis ullam magnam laboriosam quas aliquam corrupti blanditiis delectus sequi unde aspernatur vero nulla quod, natus cumque animi dolor? Officiis saepe beatae, blanditiis architecto repellendus eligendi at molestias suscipit. Voluptatibus ut dolorum debitis molestiae quas harum ea aut, deleniti voluptas incidunt quae fuga quod, nihil qui voluptatem eos perferendis repellendus! Eos quasi distinctio consequatur, recusandae sapiente saepe neque, quod corporis praesentium veritatis optio itaque inventore hic nihil vero dignissimos iusto qui? Unde voluptate quo cum velit saepe architecto nemo dolorum, quibusdam vero, quas alias veritatis porro, explicabo ea provident earum blanditiis et voluptates veniam! Facilis porro fugiat libero ipsam. Praesentium.
+                        {
+                            counting > 0 && <h1>{counting}</h1>
+                        }
+
+                        <button onClick={startCount}>Count</button>
+                        <button onClick={endCount}>Count</button>
+                    </p>
                 </div>
 
             </div>
