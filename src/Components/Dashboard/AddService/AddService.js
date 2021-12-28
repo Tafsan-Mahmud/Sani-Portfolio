bcb57142ba4513479f926cc80a917d9968b8a8a4
Tafsan Mahmud Nobin <tafsanmahmudnobin090@gmail.com>
2021-12-29 04:13:37 +0600

    90% done add service sec

--- src/Components/Dashboard/AddService/AddService.js
+++ src/Components/Dashboard/AddService/AddService.js
@@ -1,60 +1,77 @@
 import React from 'react';
 import Sidebar from '../Sidebar/Sidebar';
 import './AddService.css';
+import swal from 'sweetalert';
+import Modal from 'react-modal';
 import srvcPkCmng from './IMG_4203-01.jpg';
 import srvcPkCmng2 from './IMG_2258-02.jpg';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
+
+
+
+
+Modal.setAppElement('#root');
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transition: '.3s',
+        zindex: '222',
+        transform: 'translate(-50%, -50%)',
+    },
+    overlay: {
+        background: '#00000096'
+    }
+};
 const AddService = () => {
 
     const checkIsComing = false;
-    const todoData = JSON.parse(localStorage.getItem('TodoList'))
     const [todoFkDta, setTodoFkDta] = useState([]);
-    const [numID, setNumID] = useState(1);
-    console.log(numID)
-    console.log(todoFkDta)
+    const [serviceAllData, setServiceAllData] = useState([]);
+    const todoArry = todoFkDta.concat().reverse();
+    const [addInput, setAddInput] = useState('');
+    const [serviceNameInput, setServiceNameInput] = useState('');
 
-    // console.log(todoFkDta)
+    console.log(serviceNameInput)
+    console.log(serviceAllData)
+
+
     const handleTodoSet = () => {
-        const todo = document.getElementById('srvs-todo').value;
-        setNumID(numID + 1)
-        setTodoFkDta([...todoFkDta, { id: numID, Service: todo }])
-    }
-    useEffect(() => {
-        localStorage.setItem("TodoList", JSON.stringify(todoFkDta))
-
-    }, [todoFkDta]);
-
-
-
-    const fkSrvsData = [
-        {
-            id: 1,
-            Service: 'Youtube Marketing'
-        },
-        {
-            id: 2,
-            Service: 'Linkedin'
-        },
-        {
-            id: 3,
-            Service: 'Facebook Likes'
-        },
-        {
-            id: 4,
-            Service: 'Instagram Followers'
-        },
-        {
-            id: 5,
-            Service: 'SEO Marketing'
-        },
-        {
-            id: 6,
-            Service: 'Marketing'
+        if (!addInput) {
+            alert('Please Write Something')
+        } else {
+            const randomId = Math.floor(Math.random() * 1000000);
+            setTodoFkDta([...todoFkDta, { id: Math.floor(Math.random() * 10000), Service: addInput }])
+            setAddInput('');
         }
 
-    ];
+    }
+    const handleDeleteItem = (id) => {
+        const updateData = [...todoArry].filter(data => data.id !== id);
+        setTodoFkDta(updateData);
+    }
+    const handleStoreAllData = (e) => {
+        setServiceAllData({
+            ServiceTittle: serviceNameInput,
+            serviceImage: srvcPkCmng,
+            lisOfServices: todoFkDta
+        })
+    }
 
+    const modalon = () => {
+        setAddInput('')
+        swal({
+            title: "Warning",
+            text: "Sorry You can not add More Service List !",
+            icon: "warning",
+            // buttons: true,
+            dangerMode: true,
+        });
+    }
     return (
         <div id="addService">
             <div className="main-add-service-sec">
@@ -71,7 +88,7 @@ const AddService = () => {
                             }
 
                         </div>
-                        <form className="w-100" action='' autoComplete='off'>
+                        <form onSubmit={handleStoreAllData} className="w-100" action='' autoComplete='off'>
                             <div className="up-section">
                                 <h4>Choose service image</h4>
                                 <input type="file" name="" id="service-image" />
@@ -79,7 +96,7 @@ const AddService = () => {
                             </div>
                             <div className="add-srvc-all-input">
                                 <div className="srvc-input-data">
-                                    <input type="text" required name="Name" />
+                                    <input type="text" onChange={(e) => setServiceNameInput(e.target.value)} required name="Name" />
                                     <span>Service Name</span>
                                     <div className="underline-add-srvc"></div>
                                 </div>
@@ -89,26 +106,30 @@ const AddService = () => {
                                     <div className="underline-add-srvc"></div>
                                 </div> */}
                                 <div className="srvs-todo-add-sec">
-                                    <span>Add Services ({7 - numID})</span>
+                                    <span>Add Services ({6 - todoArry.length})</span>
                                     <div>
 
-                                        <input id='srvs-todo' type="text" />
+                                        <input id='srvs-todo' placeholder='Write Todo Services' value={addInput} onChange={(e) => setAddInput(e.target.value)} type="text" />
                                         {
-                                            numID === 7 && <button type="button" className='btn-disable' >Add</button>
+                                            todoArry.length === 6 && <button onClick={modalon} type="button" className='btn-disable' >Add</button>
                                         }
                                         {
-                                            numID < 7 && < button type="button" onClick={handleTodoSet}>Add</button>
+                                            todoArry.length < 6 && < button type="button" onClick={handleTodoSet}><i class="fas fa-plus"></i> Add</button>
                                         }
                                     </div>
                                 </div>
                                 <div className="srvs-todo-listed-sec">
                                     {
-                                        todoFkDta.map(data => <li className='srvs-todos-dsgn'>{data.id}-{data.Service}</li>
+                                        todoArry.map(data => <li key={data.id} className='srvs-todos-dsgn'>{data.Service} <i onClick={() => handleDeleteItem(data.id)} id='srvs-todo-delet-btn' class="far fa-trash-alt"></i></li>
                                         )
                                     }
                                 </div>
                             </div>
-                            <input style={{marginTop:'20px'}} type="submit" value="Submit" />
+                            <div className="srvs-sbmt-sec">
+                                <div className="srvs-sbmt-btn-sec">
+                                    <input type="submit" value="Submit" />
+                                </div>
+                            </div>
                         </form>
                     </div>
                 </div>
